d2b14dde3d841edd8e2e6dd95600f5148a9d5b0b
Nicolae Suruceanu <nicolae@Nicolaes-MacBook-Pro.local>
2025-08-10 16:34:59 -0500

    update override install.sh

--- features/macros/overrides/install.sh
+++ features/macros/overrides/install.sh
@@ -3,6 +3,12 @@ set -e
 
 SCRIPT_DIR=$(readlink -f $(dirname ${0}))
 
+test -d ~/printer_data/config/custom || mkdir -p ~/printer_data/config/custom
+
+# add the main.cfg to printer.cfg
+python ${SCRIPT_DIR}/../../../scripts/ensure_included.py \
+    ~/printer_data/config/printer.cfg custom/main.cfg
+
 test -f ~/printer_data/config/custom/main.cfg || touch ~/printer_data/config/custom/main.cfg
 
 # this file is intended to be user modified
